2d0f6006e6d766d24bb7d32b2562fae5d1f75436
Daniel BARIHUTA <dbarihut@our.ecu.edu.au>
2020-06-16 20:58:59 +0800

    Add error logging

--- epubParser.py
+++ epubParser.py
@@ -7,8 +7,14 @@ class Parser:
         self.book = {}
         self.verses = {}
         self.chap = False
+        self.__errorFile="logs/errorLog"
     def getBook(self):
-        return self.book
+        if "name" in self.book.keys():
+            return self.book
+        else:
+            logMsg="File >" + self.file.name + " does not contain verses.\n"
+            self.log('error', logMsg)
+            return "no_verses_found" 
     def getVerses(self):
         return self.verses
 
@@ -22,33 +28,39 @@ class Parser:
     def __bookMatch(self, line):
         match1 = self.titleSt.search(line) #matches pattern at beggining of string
         match2 = self.titleEnd.search(line) #searches anywhere in string
+        #print(match1, match2)
         if match1 != None and match2 != None:
             #Getting whatever is between <title> and </title>
             print("Title-match Found")
             self.match1 = match1
             self.match2 = match2
-            self.book['name'] = line[match1.span()[1]:match2.span()[0]]
-            chap_num = re.search('[0-9]+$', self.book['name'])
+            bookChap = line[match1.span()[1]:match2.span()[0]]
+            bookName = re.search('([0-9]+ )*[a-zA-z]+', bookChap)
+            chap_num = re.search('[0-9]+$', bookChap)
             if chap_num == None:
-                self.book['chapter']= 1
+                self.book['chapter'] = 1
+                self.book['name'] = bookChap
             else:
-                self.book['chapter']= self.book['name'][chap_num.span()[0]:chap_num.span()[1]]
-
+                self.book['chapter']= bookChap[chap_num.span()[0]:chap_num.span()[1]]
+                self.book['name'] = bookChap[:bookName.span()[1]]
     def __verseMatch(self, line):
         vMatch1 = self.verseSt.search(line)
 
         #Specifically search for verse end after its start.
         if vMatch1 : vMatch2 = self.verseEnd.search(line, vMatch1.span()[1])
         if vMatch1 != None and vMatch2 != None:
+            print(line)
             print("Verse found")
             vNumSlice = line[vMatch1.span()[0]:vMatch1.span()[1]]
             vNumMatch = re.search('[0-9]+', vNumSlice)
             try:
                 vNum = vNumSlice[vNumMatch.span()[0]:vNumMatch.span()[1]]
+                print(vNum)
             except:
                 print("Unexcepted None match getting vNum. Possible pattern breaking\nLine>    ", line)
             if len(self.verses)==0 and vNumMatch:
                 self.verses[1] = str(line[vMatch1.span()[1]:vMatch2.span()[0]])
+                print(self.verses)
             elif len(self.verses) !=0:
                 self.verses[str(vNum)] = str(line[vMatch1.span()[1]:vMatch2.span()[0]])
                 #Some Chapters/Books have a few verseson the same line
@@ -66,16 +78,29 @@ class Parser:
                     self.verses[str(vNum)] = str(line[vMatch1.span()[1]:vMatch2.span()[0]])
                     vMatch1 = self.verseSt.search(line, vMatch2.span()[1])
                 print(self.verses[str(vNum)])
+            print(vNum)
 
+    def log(self, logType, logMsg):
+        if logType == "error":
+            with open(self.__errorFile, 'a') as logFile:
+                logFile.write(logMsg+'\n')
 
                 
             
     def start(self):
         print("Parser Innitiated.")
         print("Search Patterns set")
-        for line in self.file.read().split('\n'):
+        #try:
+        Lines=self.file.readlines()
+        for line in Lines:
+            #print("Parsing line >", line)
             if len(self.book)==0:
                 self.__bookMatch(line)
             else:
                 self.__verseMatch(line)
-        print('Parser Done')
+        #except Exception as e:
+        #    logMsg="Exception: "+type(e).__name__+" whilst reading file >"+self.file.name + '\n'
+        #    logMsg=logMsg + "\tFurther Details> " + str(e) + "\n"
+        #    self.log("error", logMsg)
+        print('Parsing of file >'+self.file.name)
+        print('\tNumber of verses found >'+str(len(self.verses)))
--- epubTodb.py
+++ epubTodb.py
@@ -25,7 +25,7 @@ def openFile(filename, usedFile=None):
     elif (not usedFile):
         os.close(usedFile)
 
-def insertAll(values, table, cursor):
+def insertAll(cursor, values, table):
     if (table == 'books'):
         cursor.executemany('INSERT INTO book VALUES(Null,?,?,?,?,?)', values)
         print('Books inserted succefully')
@@ -35,6 +35,13 @@ def insertAll(values, table, cursor):
     if (table == 'verses'):
         cursor.executemany('INSERT INTO verse VALUES(Null,?,?,?,?)', values)
         print("Verses succesfully inserted")
+def getId(cursor, table, term):
+    if (table == 'book'):
+        result = cursor.execute('SELECT id FROM book WHERE name=?', term)
+        ID = result.fetchone()
+        print('Books inserted succefully')
+        return ID if ID else None
+
 def getVerseCount(bookId, cursor):
     result = cursor.execute("SELECT COUNT(*) FROM verse WHERE bookId=?", bookId)
     return result.fetchone()
@@ -46,8 +53,9 @@ def getAllPerTable(table, cursor):
 def main():
     books = []
     verses = {}
+    filesParsed=0
     conn = db.connect('db/bible_jw.db')
-    patFile = open('patterns.json', 'r')
+    patFile = open('patterns.json', 'r', encoding='utf8')
     patterns = json.load(patFile)
     cur = conn.cursor()
     print("Currently Supported Languages:\n    ", patterns['languages'])
@@ -61,18 +69,28 @@ def main():
     #Significantly reduces files needed to be opened
     enFilePat = re.compile(patterns['en']['files'])
     enCorrList = [f for f in enFileList if (enFilePat.match(f) != None and int(f[:2])<71)]
-
     for f in enCorrList:
-        print("reading file")
-        epubFile = open(patterns['en']['home']+f)
+        filesParsed+=1
+        print("reading file >", patterns['en']['home']+f)
+        epubFile = open(patterns['en']['home']+f, 'r', encoding='utf8')
         parser = Parser(epubFile)
 
         #change to patterns[language]
         parser.setPatterns(patterns['en'])
         parser.start()
         chapter = parser.getBook()
+        if chapter == "no_verses_found":
+            filesParsed-=1
+            continue
+        else:
+            bookId = getId(cur, table="books", term=chapter["name"])
+            if bookId:
+                insertAll
+        print(chapter)
         books.append(chapter)
         verses[chapter['name']] = parser.getVerses()
         epubFile.close()
-    print('All done')
+    cur.close()
+    conn.close()
+    print('All done. Files parsed>', filesParsed)
 main()
