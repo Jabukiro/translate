a53d28814062f47f80ff55425819dad0f6bf0141
Jabukiro <d.barihuta@gmail.com>
2020-06-18 00:16:23 +0800

    30000 verses in DB

--- epubTodb.py
+++ epubTodb.py
@@ -10,8 +10,8 @@ import sqlite3 as db
 import os
 import re
 import json
-from epubParser import Parser
-
+from xhtmlParser import Parser
+from xhtmlParser import InvalidFileError
 
 def openFile(filename, usedFile=None):
     if (int(filename[:2]) <=71 or int(filename[:2]) >= 5):
@@ -25,22 +25,32 @@ def openFile(filename, usedFile=None):
     elif (not usedFile):
         os.close(usedFile)
 
-def insertAll(cursor, values, table):
-    if (table == 'books'):
-        cursor.executemany('INSERT INTO book VALUES(Null,?,?,?,?,?)', values)
+def insertAll(cursor, table, values):
+    if (table == 'book'):
+        cursor.execute('INSERT INTO book VALUES(NULL,?,?)', values)
         print('Books inserted succefully')
+        return getId(cursor, "book", [values[0]])
+
     if (table == 'chapters'):
-        cursor.executemany('INSERT INTO chapter VALUES(Null,?,?,?)', values)
+        cursor.execute('INSERT INTO chapter ("id", "chapNum", "book_id", "descript") VALUES(null,?,?,null)', values)
         print("Chapters succesfully inserted")
+        return getId(cursor, "chapter", values)
+
     if (table == 'verses'):
-        cursor.executemany('INSERT INTO verse VALUES(Null,?,?,?,?)', values)
+        cursor.execute('INSERT INTO verses VALUES(NULL,?,?,?,?)', values)
         print("Verses succesfully inserted")
+
 def getId(cursor, table, term):
     if (table == 'book'):
         result = cursor.execute('SELECT id FROM book WHERE name=?', term)
         ID = result.fetchone()
         print('Books inserted succefully')
-        return ID if ID else None
+        return ID[0] if ID else None
+    elif (table == 'chapter'):
+        result = cursor.execute('SELECT id FROM chapter WHERE chapNum=? and book_id=?', term)
+        ID = result.fetchone()
+        print('Books inserted succefully')
+        return ID[0] if ID else None
 
 def getVerseCount(bookId, cursor):
     result = cursor.execute("SELECT COUNT(*) FROM verse WHERE bookId=?", bookId)
@@ -73,23 +83,32 @@ def main():
         filesParsed+=1
         print("reading file >", patterns['en']['home']+f)
         epubFile = open(patterns['en']['home']+f, 'r', encoding='utf8')
-        parser = Parser(epubFile)
+        parser = Parser()
 
-        #change to patterns[language]
-        parser.setPatterns(patterns['en'])
-        parser.start()
+        #Start parsing the file
+        try:
+            parser.start(epubFile)
+        except InvalidFileError:
+            continue
         chapter = parser.getBook()
         if chapter == "no_verses_found":
             filesParsed-=1
             continue
         else:
-            bookId = getId(cur, table="books", term=chapter["name"])
-            if bookId:
-                insertAll
-        print(chapter)
+            bookID = getId(cur, table="book", term=[chapter["name"]])
+            chapID = getId(cur, table="chapters", term=[chapter["chap"]])
+            if not bookID:
+                #This should not happen
+                bookID = insertAll(cur, table="book", values=[chapter["name"], "unsanitary"])
+            if not chapID:
+                chapID = insertAll(cur, table="chapters", values=[chapter["chap"], str(bookID)])
+        #print(chapter)
         books.append(chapter)
-        verses[chapter['name']] = parser.getVerses()
+        verses = parser.getVerses()
+        for versekey in verses:
+            insertAll(cur, table="verses", values=[str(chapID), str(bookID), str(versekey), verses[versekey]])
         epubFile.close()
+    conn.commit()
     cur.close()
     conn.close()
     print('All done. Files parsed>', filesParsed)
